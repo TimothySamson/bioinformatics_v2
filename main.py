d0926a453713a1aa1cde9340e3680d5d78c936c9
timothy <timothy.gab.samson@gmail.com>
2022-03-02 23:34:24 -0800

    c3w1

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-# This is a sample Python script.
+from course1.week1 import w1main
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
+def main(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
